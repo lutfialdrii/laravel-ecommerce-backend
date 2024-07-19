f9fcb4622cec311d7671c7808d975ce3231adc3e
lutfialdri <12050110415@students.uin-suska.ac.id>
2024-07-19 17:57:53 +0700

    add address + category + product

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -5,18 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Address extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'seller_id',
-        'name',
-        'description'
+        'country',
+        'province',
+        'city',
+        'district',
+        'postal_code',
+        'address',
     ];
-
-    public function products()
-    {
-        return $this->hasMany(Product::class);
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\AddressController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +16,16 @@ Route::post('/seller/register', [AuthController::class, 'registerSeller']);
 // Category
 Route::post('/seller/category', [CategoryController::class, 'store'])->middleware('auth:sanctum');
 Route::get('/seller/categories', [CategoryController::class, 'index'])->middleware('auth:sanctum');
+// Product
+Route::resource('/seller/products', ProductController::class)->middleware('auth:sanctum');
+Route::post('/seller/products/{id}', [ProductController::class, 'update'])->middleware('auth:sanctum');
+// Addresses
+Route::resource('buyer/addresses', AddressController::class)->middleware('auth:sanctum');
 
+
+// [ auth ]
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-// buyer
+// [ buyer ]
 Route::post('/buyer/register', [AuthController::class, 'registerBuyer']);
